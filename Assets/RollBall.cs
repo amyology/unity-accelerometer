06140b776077e72eb97800e07c1d024a874f384d
Amy Chan <chamy09@gmail.com>
2016-12-12 16:45:39 -0600

    initial commit

---
+++ Assets/RollBall.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollBall : MonoBehaviour {
+
+	public Rigidbody rb;
+	public int speed = 10;
+	float moveHorizontal;
+	float moveVertical;
+
+	// Use this for initialization
+	void Start () {
+
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveHorizontal = Input.acceleration.x;
+		moveVertical = Input.acceleration.y;                        
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed * 2);
+	}
+}
